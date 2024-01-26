fdce7e8fb4806d139907ff0d8aabf2d0391aae4f
mintchoc31 <mintchoc31@gmail.com>
2024-01-26 14:05:03 +0900

    exception handler 기본 코드 완료

--- src/main/java/com/tenco/bank/controller/TestController.java
+++ src/main/java/com/tenco/bank/controller/TestController.java
@@ -1,8 +1,11 @@
 package com.tenco.bank.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.tenco.bank.handler.exception.CustomRestfulException;
 
 @Controller
 @RequestMapping("/test1") // 대문
@@ -11,12 +14,16 @@ public class TestController {
 	// 주소 설계
 	// http://localhost:80(기본 포트라 80 생략가능)/test/main
 	@GetMapping("/main")
-	public String mainPage() {
+	public void mainPage() {
+		System.out.println("1111");
 		// 인증 검사 
 		// 유효성 검사 
 		// 뷰 리졸브 --> 해당하는 파일 찾아 (data)
 		// yml에 prefix, suffix를 설정 했기 때문에 web-inf~(pre-)/~.jsp(suf-)는 자동으로 설정됨
-		return "layout/main";
+		
+		// 예외 발생
+		throw new CustomRestfulException("페이지가 없네요", HttpStatus.NOT_FOUND);
+		// return "layout/main";
 	}
 
 }
---
+++ src/main/java/com/tenco/bank/handler/MyRestfulExceptionHandler.java
@@ -0,0 +1,34 @@
+package com.tenco.bank.handler;
+
+import org.springframework.core.annotation.Order;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.tenco.bank.handler.exception.CustomRestfulException;
+
+@Order(1)
+@RestControllerAdvice
+public class MyRestfulExceptionHandler {
+
+	// 모든 예외 클래스 설정
+	@ExceptionHandler(Exception.class)
+	public void exception(Exception e) {
+		System.out.println("---------");
+		System.out.println(e.getClass().getName());
+		System.out.println(e.getMessage());
+		System.out.println("---------");
+	}
+	/*
+	String str = "aaaa";
+	String str2 = new String("aaaa");
+	*/
+	
+	@ExceptionHandler(CustomRestfulException.class)
+	public String basicException(CustomRestfulException e) {
+		StringBuffer sb = new StringBuffer();	
+		sb.append("<script>");
+		sb.append("alert('" + e.getMessage() + "');");		
+		sb.append("</script>");
+		return sb.toString();
+	}
+}
